36f4f3ea423c1fc4b815c2794373a92f4c95e97c
Alexander Koval <1667444+alexander-koval@users.noreply.github.com>
2024-10-09 15:32:31 +0700

    Alembic initialization

--- carmain/models/users.py
+++ carmain/models/users.py
@@ -1,11 +1,12 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable
-from sqlalchemy import String
+from sqlalchemy import String, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 from carmain.backend.db import Base
 
 
 class User(SQLAlchemyBaseUserTable, Base):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     first_name: Mapped[str] = mapped_column(String)
     last_name: Mapped[str] = mapped_column(String)
-    username: Mapped[str] = mapped_column(String, unique=True)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True, nullable=False)
