3f53503b2d68a44ba27776ac5e97718fd2756999
Alexander Koval <1667444+alexander-koval@users.noreply.github.com>
2024-10-11 19:42:29 +0700

    little updates

--- carmain/admin/users.py
+++ carmain/admin/users.py
@@ -1,7 +1,12 @@
 from sqladmin import ModelView
 
+from carmain.models.auth import AccessToken
 from carmain.models.users import User
 
 
 class UserAdmin(ModelView, model=User):
     column_list = [User.id, User.email]
+
+
+class AccessTokenAdmin(ModelView, model=AccessToken):
+    column_list = [AccessToken.user_id, AccessToken.token, AccessToken.data]
--- carmain/main.py
+++ carmain/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, Request
+from fastapi_users.router import get_auth_router
 from sqladmin import Admin
 from starlette.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from starlette.middleware.trustedhost import TrustedHostMiddleware
 
-from carmain.admin.users import UserAdmin
+from carmain.admin.users import UserAdmin, AccessTokenAdmin
 from carmain.core.db import engine, settings
 from carmain.routers import auth
 
@@ -12,17 +13,19 @@ carmain = FastAPI(title="Carmain", debug=True)
 
 admin = Admin(carmain, engine=engine)
 admin.add_view(UserAdmin)
+admin.add_view(AccessTokenAdmin)
 
 carmain.include_router(auth.router)
+# carmain.include_router(get_auth_router())
 
 
-# @carmain.middleware("http")
-# async def validate_user(request: Request, call_next):
-#     print(
-#         request.session
-#     )  # <--- Error: 'AssertionError: SessionMiddleware must be installed to access request.session'
-#     response = await call_next(request)
-#     return response
+@carmain.middleware("http")
+async def validate_user(request: Request, call_next):
+    print(
+        request.session
+    )  # <--- Error: 'AssertionError: SessionMiddleware must be installed to access request.session'
+    response = await call_next(request)
+    return response
 
 
 @carmain.get("/")
@@ -30,6 +33,6 @@ async def welcome(request: Request) -> dict:
     return {"message": f"Welcome {request.session}"}
 
 
-# carmain.add_middleware(SessionMiddleware, secret_key=settings.secret_key)
+carmain.add_middleware(SessionMiddleware, secret_key=settings.secret_key)
 # carmain.add_middleware(CORSMiddleware, allow_origins=["*"])
 # carmain.add_middleware(TrustedHostMiddleware, allowed_hosts=["127.0.0.1", "localhost"])
